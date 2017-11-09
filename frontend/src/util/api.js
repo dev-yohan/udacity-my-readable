9386a6c23b93e5f977d3743241c61ee2c99a5cf3
yohan morales <yohanmorales@hotmail.com>
2017-11-08 19:36:22 -0500

    type: refactor remove duplicate code in API calls

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -6,8 +6,32 @@ const headers = {
   'Content-type':  'application/json'
 }
 
+function getRequest(path) {
+  return fetch(path, { headers })
+}
+
+function postRequest(path, body) {
+  return fetch(path, { 
+    method: 'post',
+    body:    JSON.stringify(body),
+    headers: headers })
+}
+
+function putRequest(path, body) {
+  return fetch(path, { 
+    method: 'put',
+    body:    JSON.stringify(body),
+    headers: headers })
+}
+
+function deleteRequest(path) {
+  return fetch(path, { 
+    method: 'delete',
+    headers: headers })
+}
+
 export function  getCategories () {
-  return fetch(`${CONTENT_API}/categories`, { headers })
+  return getRequest(`${CONTENT_API}/categories`)
     .then((res) => {
       return res.json()
     })
@@ -17,7 +41,7 @@ export function  getCategories () {
 }
 
 export function getPosts() {
-  return fetch(`${CONTENT_API}/posts`, { headers })
+  return getRequest(`${CONTENT_API}/posts`)
     .then((res) => {
       return res.json()
     })
@@ -27,7 +51,7 @@ export function getPosts() {
 }
 
 export function getPostsByCategory(category) {
-  return fetch(`${CONTENT_API}/${category}/posts`, { headers })
+  return getRequest(`${CONTENT_API}/${category}/posts`)
     .then((res) => {
       return res.json()
     })
@@ -37,7 +61,7 @@ export function getPostsByCategory(category) {
 }
 
 export function getPost(id) {
-  return fetch(`${CONTENT_API}/posts/${id}`, { headers })
+  return getRequest(`${CONTENT_API}/posts/${id}`)
     .then((res) => {
       return res.json()
     })
@@ -47,7 +71,7 @@ export function getPost(id) {
 }
 
 export function getPostComments(id) {
-  return fetch(`${CONTENT_API}/posts/${id}/comments`, { headers })
+  return getRequest(`${CONTENT_API}/posts/${id}/comments`)
     .then((res) => {
       return res.json()
     })
@@ -57,10 +81,7 @@ export function getPostComments(id) {
 }
 
 export function addPost(body) {
-  return fetch(`${CONTENT_API}/posts`, { 
-    method: 'post',
-    body:    JSON.stringify(body),
-    headers: headers })
+  return postRequest(`${CONTENT_API}/posts`, body)  
   .then((res) => {
     return res.json()
   })
@@ -70,10 +91,7 @@ export function addPost(body) {
 }
 
 export function editPost(id, body) {
-  return fetch(`${CONTENT_API}/posts/${id}`, { 
-    method: 'put',
-    body:    JSON.stringify(body),
-    headers: headers })
+  return putRequest(`${CONTENT_API}/posts/${id}`, body)  
   .then((res) => {
     return res.json()
   })
@@ -83,9 +101,7 @@ export function editPost(id, body) {
 }
 
 export function deletePost(id) {
-  return fetch(`${CONTENT_API}/posts/${id}`, { 
-    method: 'delete',
-    headers: headers })
+  return deleteRequest(`${CONTENT_API}/posts/${id}`)  
   .then((res) => {
     return res.json()
   })
@@ -95,10 +111,7 @@ export function deletePost(id) {
 }
 
 export function addPostComment(body) {
-  return fetch(`${CONTENT_API}/comments`, { 
-    method: 'post',
-    body:   JSON.stringify(body),
-    headers: headers })
+  return postRequest(`${CONTENT_API}/comments`, body)
   .then((res) => {
     return res.json()
   })
@@ -108,10 +121,7 @@ export function addPostComment(body) {
 }
 
 export function addPostVote(postId, body) {
-  return fetch(`${CONTENT_API}/posts/${postId}`, { 
-    method: 'post',
-    body:   JSON.stringify(body),
-    headers: headers })
+  return postRequest(`${CONTENT_API}/posts/${postId}`, body)
   .then((res) => {
     return res.json()
   })
@@ -121,10 +131,7 @@ export function addPostVote(postId, body) {
 }
 
 export function addCommentVote(commentId, body) {
-  return fetch(`${CONTENT_API}/comments/${commentId}`, { 
-    method: 'post',
-    body:   JSON.stringify(body),
-    headers: headers })
+  return postRequest(`${CONTENT_API}/comments/${commentId}`, body)
   .then((res) => {
     return res.json()
   })
@@ -134,9 +141,7 @@ export function addCommentVote(commentId, body) {
 }
 
 export function deleteComment(id) {
-  return fetch(`${CONTENT_API}/comments/${id}`, { 
-    method: 'delete',
-    headers: headers })
+  return deleteRequest(`${CONTENT_API}/comments/${id}`)
   .then((res) => {
     return res.json()
   })
@@ -146,10 +151,7 @@ export function deleteComment(id) {
 }
 
 export function editComment(id, body) {
-  return fetch(`${CONTENT_API}/comments/${id}`, { 
-    method: 'put',
-    body:   JSON.stringify(body),
-    headers: headers })
+  return putRequest(`${CONTENT_API}/comments/${id}`, body)
   .then((res) => {
     return res.json()
   })
